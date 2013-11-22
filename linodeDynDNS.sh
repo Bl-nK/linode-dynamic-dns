ecf774671c14edadf8461186c60a48a5f2abeb2d
Brandon Howard <brandon@bl-nk.net>
2013-11-22 15:08:42 -0600

    Initial Commit

---
+++ linodeDynDNS.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#Usage ./linodeDynDNS.sh domainID resourceID API_Key ip_site
+domainID=$1
+resourceID=$2
+api_key=$3
+ip_site=$4
+
+#Retrieve current public IP address
+current_IP=$(curl -s $ip_site)
+
+#Current DNS Record
+current_dns=$(curl -s "https://api.linode.com/?api_key=$api_key&api_action=domain.resource.list&DomainID=$domainID&ResourceID=$resourceID")
+
+#Determine if current_dns is the same as current_IP
+if [[ $(echo $current_dns | grep $current_IP) ]]; then
+	exit 0
+else
+	#Update address to current_IP
+	curl -s "https://api.linode.com/?api_key=$api_key&api_action=domain.resource.update&DomainID=$domainID&ResourceID=$resourceID&Target=$current_IP" > /dev/null
+fi
